eeb1c94b9206cb3086641c5d928a7db7302461e3
thomasIRA <thomas.al@live.ca>
2023-06-26 21:12:51 -0400

    'broken skew'-good start

--- assets/script/Card.js
+++ assets/script/Card.js
@@ -4,24 +4,43 @@ class Card{
         this._el = el;
         this._opacity = opacity;
 
-        this._el.innerHTML = `<div data-js-hover></div>`;
+        this._el.innerHTML = `<div data-js-hover></div><div data-js-content></div>`;
+
         this._hover = this._el.querySelector('[data-js-hover]');
+        this._content = this._el.querySelector('[data-js-content]');
+
+        this._el.style.borderRadius = `20px`;
+        this._el.style.perspective = `700px`;
 
         this._hover.style.width = `${size}px`;
         this._hover.style.height = `${size}px`;
         this._hover.style.opacity = `0`;
+        this._hover.style.zIndex = `1`;
         this._hover.style.borderRadius = `100%`;
         this._hover.style.position = `absolute`;
         this._hover.style.backgroundColor = color;
-        this._hover.style.filter = `blur(${blur}px)`;
+        this._hover.style.filter = `blur(${blur})`;
+
+        this._content.style.border = `1px solid rgba(220, 220, 220, 0.8)`;
+        this._content.style.width = `100%`;
+        this._content.style.height = `100%`;
+        this._content.style.zIndex = `-1`;
+        this._content.style.borderRadius = `20px`;
+        this._content.style.backgroundColor = `rgb(41, 41, 56)`;
+    
+
     }
     isHovered(bool, top, left){
         if(bool){
+            this._content.style.transform = `rotateX(11deg) rotateY(11deg)`;
             this._hover.style.top = `${top}px`;
             this._hover.style.left = `${left}px`;
             this._hover.style.opacity = this._opacity;
             this._hover.style.transitionDuration = `.7s`;
             this._hover.style.transitionProperty = `opacity`;
+
+
+            
 
         } else {
             this._hover.style.opacity = `0`;
--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,61 @@
 window.addEventListener('DOMContentLoaded', function(){
     
-    const elMain = document.querySelector('main');
+    const elMain = document.querySelector('main'),
+        elsCard = elMain.querySelectorAll('[data-js-card]'),
+        aCards = [];
 
-    let hoverSize = 400,
-        hoverColor = 'red',
-        card = new Card(elMain.querySelector('[data-js-card'), hoverSize, hoverColor, '0.4', 200);
+    let hoverSize = '600',
+        hoverColor = 'rgb(0, 255, 21)',
+        hoverOpacity = '.2',
+        hoverBlur = '200px';
+    
+        for (let i = 0; i < elsCard.length; i++) {
+            
+            aCards[i] = new Card(elsCard[i], hoverSize, hoverColor, hoverOpacity, hoverBlur);
 
+            aCards[i]._el.addEventListener('mousemove', function(e){
+
+                const cardPosition = elsCard[i].getBoundingClientRect();
+
+                aCards[i].isHovered(true, (e.pageY-cardPosition.top-hoverSize/2), (e.pageX-cardPosition.left-hoverSize/2));
+
+            })  
+            aCards[i]._el.addEventListener('mouseout', function(){
+        
+                aCards[i].isHovered(false)
+        
+            })
+
+
+        }
+        console.log(aCards)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* card = new Card(elMain.querySelector('[data-js-card'), hoverSize, hoverColor, hoverOpacity, hoverBlur); */
+
+    
+
+
+
+/* 
     let cardPosition = card._el.getBoundingClientRect();
 
 
@@ -19,7 +69,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
         card.isHovered(false)
 
-    })
+    }) */
 
 
 })
